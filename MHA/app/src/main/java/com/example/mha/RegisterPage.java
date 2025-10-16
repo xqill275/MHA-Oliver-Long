320c71be30616fcbcd3db4cc72619f1c1b5552ac
xqill275 <oliverlong275@gmail.com>
2025-10-16 11:44:25 +0100

    Layout and backbutton for reg pape

--- MHA/app/src/main/java/com/example/mha/RegisterPage.java
+++ MHA/app/src/main/java/com/example/mha/RegisterPage.java
@@ -11,26 +11,25 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-    Button registerBtn;
+public class RegisterPage extends AppCompatActivity {
+    Button BackBtn, RegisterBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register_page);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        BackBtn = findViewById(R.id.Back_Button);
+        RegisterBtn = findViewById(R.id.Register_button);
 
-        registerBtn = findViewById(R.id.RegisterButton);
-
-        registerBtn.setOnClickListener(new View.OnClickListener() {
+        BackBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, RegisterPage.class));
-
+                startActivity(new Intent(RegisterPage.this, MainActivity.class));
             }
         });
     }
