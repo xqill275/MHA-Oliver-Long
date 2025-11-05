be418109d5149876b7bfd464421239161f3e1479
xqill275 <oliverlong275@gmail.com>
2025-11-05 14:43:00 +0000

    users can now book apointments

--- MHA/app/src/main/java/com/example/mha/ApointmentActivity.java
+++ MHA/app/src/main/java/com/example/mha/ApointmentActivity.java
@@ -1,6 +1,8 @@
 package com.example.mha;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +11,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class ApointmentActivity extends AppCompatActivity {
-
+    Button ViewBtn, BookBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +22,19 @@ public class ApointmentActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        int userId = getIntent().getIntExtra("UserId", -1);
+
+        ViewBtn = findViewById(R.id.ViewBookingButton);
+        BookBtn = findViewById(R.id.BookButton);
+
+        BookBtn.setOnClickListener(v -> {
+            Intent Apointintent = new Intent(ApointmentActivity.this, BookActivity.class);
+            Apointintent.putExtra("UserId", userId);
+            startActivity(Apointintent);
+        });
+
+
     }
+
 }
