8529de280b8cfc6fe1cc94d88ef314112436e3e7
xqill275 <oliverlong275@gmail.com>
2025-11-18 14:05:52 +0000

    forgot about the barcode scanner it works now

--- MHA/app/build.gradle.kts
+++ MHA/app/build.gradle.kts
@@ -47,4 +47,11 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:3.0.0")
     implementation("com.squareup.retrofit2:converter-gson:3.0.0")
     implementation("com.squareup.okhttp3:logging-interceptor:5.2.1")
+    implementation("com.google.mlkit:barcode-scanning:17.2.0")
+    implementation("com.google.mlkit:vision-common:17.3.0")
+    implementation("com.google.android.gms:play-services-mlkit-barcode-scanning:18.2.0")
+    implementation("androidx.camera:camera-core:1.3.1")
+    implementation("androidx.camera:camera-camera2:1.3.1")
+    implementation("androidx.camera:camera-lifecycle:1.3.1")
+    implementation("androidx.camera:camera-view:1.3.1")
 }
